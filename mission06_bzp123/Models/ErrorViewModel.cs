bc3d49be3f3b1988a94e14acea6f2a5ae9a1975c
busbyberkly <bzpeterson0901@gmail.com>
2023-02-07 20:04:32 -0700

    Add project files.

---
+++ mission06_bzp123/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mission06_bzp123.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
