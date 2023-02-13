8b30960d95d381b167983fba19305925ca3aef0c
busbyberkly <bzpeterson0901@gmail.com>
2023-02-12 23:33:16 -0700

    configure connection

--- mission06_bzp123/Startup.cs
+++ mission06_bzp123/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using mission06_bzp123.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,11 @@ namespace mission06_bzp123
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MovieEntryContex>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:DataConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
