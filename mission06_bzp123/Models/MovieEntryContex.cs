ebf3c43adcac484f2fbc80e66077e0cc4f5d2c41
busbyberkly <bzpeterson0901@gmail.com>
2023-02-12 23:20:24 -0700

    Setting up DbContext

---
+++ mission06_bzp123/Models/MovieEntryContex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission06_bzp123.Models
+{
+    public class MovieEntryContex : DbContext
+    {
+        //this is my contructor, only gets built a singular time
+        public MovieEntryContex (DbContextOptions<MovieEntryContex>options) : base (options)
+        {
+            //leave blank for now
+        }
+
+        public DbSet<NewMovieForm> newMovies { get; set; }
+    }
+}
